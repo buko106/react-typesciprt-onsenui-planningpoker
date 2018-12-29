177f35930a38830e01c5835dd9f813aa3660964d
buko106 <buko1062000@yahoo.co.jp>
2018-12-29 22:29:31 +0900

    implement filtering out inactive member

--- src/pages/RoomDetail.tsx
+++ src/pages/RoomDetail.tsx
@@ -4,6 +4,7 @@ import * as firebase from 'firebase/app';
 import { Member, Room } from '../object-types/object-types';
 import { BackButton, List, ListItem, Navigator, Page, Toolbar } from 'react-onsenui';
 import { interval, Subscription } from 'rxjs';
+import { getTimeOffsetFromDatabaseAsync } from './utils';
 
 interface Props {
   roomKey: string;
@@ -14,6 +15,10 @@ interface Props {
 
 interface State {
   room: Room | undefined;
+}
+
+interface MemberStats extends Member {
+  key: string;
 }
 
 export default class RoomDetail extends Component<Props, State> {
@@ -30,6 +35,7 @@ export default class RoomDetail extends Component<Props, State> {
   }
 
   private intervalSubscription?: Subscription;
+  private serverTimeOffset: number = 0;
 
   async componentDidMount() {
     this.roomRef.on('value', snapshot => {
@@ -40,11 +46,13 @@ export default class RoomDetail extends Component<Props, State> {
       const room = snapshot.toJSON() as Room;
       this.setState({room});
     });
+    this.serverTimeOffset = await getTimeOffsetFromDatabaseAsync(this.props.database);
 
     await this.myPresenceRef.set({
       last_seen_at: firebase.database.ServerValue.TIMESTAMP,
       joined_at: firebase.database.ServerValue.TIMESTAMP,
       display_name: 'todo(name)',
+      card_choice: 'one',
     } as Member);
 
     this.intervalSubscription = interval(1000).subscribe(() => {
@@ -77,16 +85,46 @@ export default class RoomDetail extends Component<Props, State> {
     )
   };
 
-  render() {
+  private renderList() {
+    const loading = () => (
+      <List>
+        <ListItem>
+          読み込み中...
+        </ListItem>
+      </List>
+    );
+
+
     const {room} = this.state;
+    const memberKeys: string[] = room ? Object.keys(room.members || {}) : [];
+    if (memberKeys.length == 0) {
+      return loading();
+    }
+
+    const serverTimestamp = new Date().getTime() + this.serverTimeOffset;
+    const MEMBER_INACTIVITY_THRESHOLD_MILLISECOND = 1000;  // 1 sec.
+    const members = memberKeys.map(key => {
+      const member: Member = room!.members![key];
+      return {...member, key};
+    }).filter(
+      (member: MemberStats) => (member.last_seen_at >= serverTimestamp - MEMBER_INACTIVITY_THRESHOLD_MILLISECOND)
+    );
 
     return (
-      <Page renderToolbar={this.renderToolbar}>
-        <List>
-          <ListItem>
-            {JSON.stringify(room)}
+      <List>
+        {members.map((member: MemberStats) => (
+          <ListItem key={member.key}>
+            {member.card_choice} / {member.display_name}
           </ListItem>
-        </List>
+        ))}
+      </List>
+    );
+  }
+
+  render() {
+    return (
+      <Page renderToolbar={this.renderToolbar}>
+        {this.renderList()}
       </Page>
     );
   }
---
+++ src/pages/utils.ts
@@ -0,0 +1,5 @@
+import * as firebase from 'firebase/app';
+
+export const getTimeOffsetFromDatabaseAsync = async (database: firebase.database.Database): Promise<number> => {
+  return (await database.ref('.info/serverTimeOffset').once('value')).val();
+};
